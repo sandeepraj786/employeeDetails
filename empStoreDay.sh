5b222318b3b0f44401e0fbae3b0efe9a5bf40555
sandeep786 <sadeeprajsingh05@gmail.com>
2020-08-13 21:50:37 +0530

    added to store day to empWage

---
+++ empStoreDay.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+echo Welcome To Employee Wage problem.
+
+WAGEPERHOUR=20
+HOURCONDITION=100
+MONTH=20
+day=1
+hour=0
+
+function workHour() {
+	case $1 in
+			1)
+				workingHour=8
+			;;
+			2)
+				workingHour=4
+			;;
+			*)
+				workingHour=0
+			;;
+		esac
+		echo $workingHour
+}
+function dailyWage() {
+	local timeHour=$1
+	dailyIncome=$(($timeHour*$WAGEPERHOUR))
+	echo $dailyIncome
+}
+while(( day<=MONTH && hour<HOURCONDITION ))
+do
+	randomValue=$((RANDOM%3))
+	emphour=$(workHour $randomValue)
+	hour=$(($hour+$emphour))
+	wageArray[day]=$(dailyWage $emphour)
+	((day++))
+done
+
+salary=$(($WAGEPERHOUR*$hour))
+	echo "Total Salary will be: $salary"
+	echo "Daily income: ${wageArray[@]}"
+	echo "Days: ${!wageArray[@]}"
